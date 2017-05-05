f3415ab7077f687365e68aee8faac60691f13b80
ssashida <sashidapc373443@gmail.com>
2017-05-05 19:27:52 +0900

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,31 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+int main(int argc, char** argv)
+{
+	auto app = new ofApp();
+
+	int width = 1600, height = 900;
+	if (argc >= 3)
+	{
+		int width = std::stoi(argv[1]);
+		int height = std::stoi(argv[2]);
+	}
+	// ofSetupOpenGL(width, height, OF_FULLSCREEN);			// <-------- setup the GL context
+	ofSetupOpenGL(width, height, OF_WINDOW);			// <-------- setup the GL context
+
+	std::string folderPath = R"(.\img)";
+
+	if (argc >= 4)
+	{
+		folderPath = std::string(argv[3]);
+	}
+
+	app->SetFolderPath(folderPath);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp(new ofApp());
+	ofRunApp(app);
 
 }
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,18 +1,161 @@
 #include "ofApp.h"
+#include <algorithm>
+#include <chrono>
+using namespace std::chrono;
+
+
+namespace {
+	std::vector<std::string> GetFiles(const char* folderPath)
+	{
+		std::vector<std::string> out;
+
+		char path[MAX_PATH];
+		WIN32_FIND_DATAA ffd;
+
+		if (strlen(folderPath) > MAX_PATH - 4)
+		{
+			return out;
+		}
+
+		strcpy(path, folderPath);
+		if (path[strlen(path) - 1] != '\\')
+			strcat(path, "\\");
+		strcat(path, "*.*");
+
+		HANDLE hFind = FindFirstFileA(path, &ffd);
+		if (hFind == INVALID_HANDLE_VALUE)
+		{
+			return out;
+		}
+
+		bool finished = false;
+		while (!finished)
+		{
+
+			if (strcmp(ffd.cFileName, ".") != 0 &&
+				strcmp(ffd.cFileName, "..") != 0)
+			{
+				out.push_back(ffd.cFileName);
+			}
+			
+			if (!FindNextFileA(hFind, &ffd))
+			{
+				finished = true;
+			}
+		}
+		return out;
+	}
+
+	std::string NarrowString(const std::wstring& str, const char* localeName = "japanese")
+	{
+		std::string result;
+		result.resize(str.size());
+
+		std::locale loc(localeName);
+
+		std::use_facet<std::ctype<wchar_t> >(loc).narrow(
+			str.c_str(), str.c_str() + str.size(), '?', &*result.begin());
+
+		return result;
+	}
+}
+
+struct PlayInfo
+{
+	size_t fileNum;
+	std::vector<string> fileNames;
+	std::vector<double> times;
+
+	std::vector<string>::const_iterator itName;
+	std::vector<double>::const_iterator itTime;
+
+	system_clock::time_point curr;
+	system_clock::time_point prev;
+
+
+	ofImage img;
+
+	size_t duration;	//!< millis
+};
+
+static PlayInfo playInfo;
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup()
+{
+	auto files = GetFiles(m_folderPath.c_str());
+	std::random_shuffle(files.begin(), files.end());
 
+	playInfo.fileNum = files.size();
+	playInfo.fileNames = files;
+	// トータル再生時間（ミリ秒）
+	playInfo.duration = 30 * 60 * 1000;
+
+	ofSeedRandom(12345);
+	for (size_t i = 0; i < playInfo.fileNum; ++i)
+	{
+		// 適当に画像の表示長さを決定
+		double v = ofRandom(0.0, 8.0);
+		cout << "noise = " << v << endl;
+
+		playInfo.times.push_back(v);
+	}
+
+	double sum = std::accumulate(playInfo.times.begin(), playInfo.times.end(), 0.0);
+
+	double rate = playInfo.duration / sum;
+
+	// 尺に合うように各画像の再生時間を調整
+	for (double& d : playInfo.times)
+	{
+		d *= rate;
+	}
+
+	// 確認
+	double sum2 = std::accumulate(playInfo.times.begin(), playInfo.times.end(), 0.0);
+
+	playInfo.itName = playInfo.fileNames.begin();
+	playInfo.itTime = playInfo.times.begin();
+
+	playInfo.curr = system_clock::now();
+
+	
+	auto path = m_folderPath + "\\" + *(playInfo.itName);
+	std::cout << path << std::endl;
+	playInfo.img.load(path);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 
+	auto span = system_clock::now() - playInfo.curr;
+	auto millis = duration_cast<milliseconds>(span).count();
+
+	if (millis > *(playInfo.itTime))
+	{
+		std::cout << "(s:" << millis << ", " << *(playInfo.itTime) << ")" << std::endl;
+		playInfo.curr = system_clock::now();
+		++(playInfo.itTime);
+		++(playInfo.itName);
+		playInfo.img.load(m_folderPath + "\\" + *(playInfo.itName));
+
+		std::cout << "fname = " << *(playInfo.itName) << std::endl << std::endl;
+	}
+
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	ofClear(ofColor::white);
 
+	ofPoint wsz = ofGetWindowSize();
+
+	int w = wsz.x;
+	int h = wsz.x * (9.0 / 16.0);
+
+	int t = (wsz.y - h) / 2;
+	
+	playInfo.img.draw(0, t, w, h);
 }
 
 //--------------------------------------------------------------
@@ -69,3 +212,9 @@ void ofApp::gotMessage(ofMessage msg){
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
 }
+
+void ofApp::SetFolderPath(std::string folderPath)
+{
+	m_folderPath = folderPath;
+}
+
